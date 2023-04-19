9a12c2a50b85f917c529fa1f8dfedae0fc87a09e
tnguyen-win <129979622+tnguyen-win@users.noreply.github.com>
2023-04-18 20:33:02 -0700

    Lab 2 - Draft - StringReverseStack

--- DataStructures_Core5/StringReverseStack/Program.cs
+++ DataStructures_Core5/StringReverseStack/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace StringReverseStack
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace StringReverseStack {
+    class Program {
+        static void Main() {
+            Console.Write("Enter a sentence/phrase: ");
+
+            string promptString = Console.ReadLine();
+
+            Stack<char> stackOfStrings = new Stack<char>();
+
+            for (int s = 0; s < promptString.Length; s++) stackOfStrings.Push(promptString[s]);
+
+            Console.WriteLine($"\n\nHere is your sentence in reverse:");
+
+            foreach (char str in stackOfStrings) Console.Write(str);
+
+            Console.WriteLine("\n");
         }
     }
 }
